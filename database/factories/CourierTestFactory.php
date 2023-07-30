f9eb091276fec90501aabaa12c0d77663aa86622
fanajib5 <34987164+fanajib5@users.noreply.github.com>
2023-07-30 13:18:52 +0700

    mengubah format date dari DOB dan DOJ

--- database/factories/CourierTestFactory.php
+++ database/factories/CourierTestFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ *  //@extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
 class CourierTestFactory extends Factory
 {
@@ -16,14 +16,17 @@ class CourierTestFactory extends Factory
      */
     public function definition(): array
     {
+        $dobRange = fake()->dateTimeBetween('1989-01-01', '1998-12-31');
+        $dojRange = fake()->dateTimeBetween('2019-01-01', '2023-03-20');
+
         return [
             'email' => fake('id_ID')->unique()->safeEmail(),
             'name' => fake('id_ID')->name(),
-            'DOB' => fake()->date(),
+            'DOB' => date_format($dobRange, "Y-m-d"),
             'created_at' => now(),
             'phone' => fake('id_ID')->phoneNumber(),
             'status' => fake()->randomElement(['Active', 'Disabled']),
-            'DOJ' => fake()->date(),
+            'DOJ' => date_format($dojRange, "Y-m-d"),
             'level' => fake()->numberBetween(1,5),
             'branch_id' => fake()->numberBetween(1,25),
         ];
